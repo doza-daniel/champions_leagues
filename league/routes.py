0ed9b7eb515a83cf28688bc2548e76e77aedb0ad
Daniel Doza <doza.daniel@gmail.com>
2019-04-08 20:40:31 +0200

    placeholder routes for login/register/logout

--- league/routes.py
+++ league/routes.py
@@ -1,7 +1,20 @@
 from flask import render_template
+from league import app
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/login")
+def login():
+    pass
+
+@app.route("/logout")
+def logout():
+    pass
+
+@app.route("/register")
+def register():
+    pass
 
