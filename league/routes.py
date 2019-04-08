0060621d18f7c57b4c80079417c7acf7127b283e
Daniel Doza <doza.daniel@gmail.com>
2019-04-08 21:01:15 +0200

    registration route implemented

--- league/routes.py
+++ league/routes.py
@@ -1,5 +1,9 @@
-from flask import render_template
-from league import app
+from flask import render_template, url_for, redirect, flash
+from flask_login import current_user
+
+from league import app, db, bcrypt
+from league.forms import RegistrationForm
+from league.models import User
 
 @app.route("/")
 @app.route("/home")
@@ -14,7 +18,17 @@ def login():
 def logout():
     pass
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    pass
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
