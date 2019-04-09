bdd9f3b1a883694a57f0f10516083b163f51c1a5
Daniel Doza <doza.daniel@gmail.com>
2019-04-09 02:25:13 +0200

    create_league form added

--- league/forms.py
+++ league/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,  IntegerField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from datetime import date
 from league.models import User
 
 
@@ -33,3 +35,11 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+
+class CreateLeagueForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
+    date_started = DateField('Start Date', default=date.today(), format='%Y-%m-%d')
+    number_of_players = IntegerField('Number of players', validators=[DataRequired()])
+    group_size = IntegerField('Group Size', validators=[DataRequired()])
+
+    submit = SubmitField('Create league')
