7f83c9221aaf0979f8c560e5263d882ad56f65dc
Daniel Doza <doza.daniel@gmail.com>
2019-04-09 02:27:23 +0200

    route for create league added

--- league/routes.py
+++ league/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, url_for, redirect, flash
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 from league import app, db, bcrypt
-from league.forms import RegistrationForm, LoginForm
-from league.models import User
+from league.forms import RegistrationForm, LoginForm, CreateLeagueForm
+from league.models import User, League, Group
 
 @app.route("/")
 @app.route("/home")
@@ -43,3 +43,30 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route("/create_league", methods=['GET', 'POST'])
+@login_required
+def create_league():
+    form = CreateLeagueForm()
+    if form.validate_on_submit():
+        nplayers = form.number_of_players.data
+
+        league = League(
+            owner_id=current_user.id,
+            name=form.name.data,
+            date_started=form.date_started.data,
+            number_of_players=nplayers
+        )
+
+        group_size = form.group_size.data
+        ngroups = divmod(nplayers, group_size)
+
+        for i in range(0, ngroups[0] if ngroups[1] == 0 else ngroups[0] + 1):
+            league.groups.append(Group(league_id=league.id, size=group_size))
+
+        db.session.add(league)
+        db.session.commit()
+
+        flash(f"League '{league.name}' has been created successfully!", 'success')
+        return redirect(url_for('home'))
+
+    return render_template('create_league.html', title='Create League', form=form)
