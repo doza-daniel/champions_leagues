b27231be63d4b3bc8f415e34fb53f48897213423
Daniel Doza <doza.daniel@gmail.com>
2019-04-11 17:30:19 +0200

    edit league forms

--- league/forms.py
+++ league/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField,  IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,  IntegerField, SelectMultipleField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from datetime import date
@@ -40,6 +40,29 @@ class CreateLeagueForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
 
     submit = SubmitField('Create league')
+
+class StartLeagueForm(FlaskForm):
+    def __init__(self, *args, **kwargs):
+        super(StartLeagueForm, self).__init__(*args, **kwargs)
+
+    date_started = DateField('Start Date', default=date.today(), format='%Y-%m-%d')
+    start = SubmitField('Start league')
+
+class RemovePlayerForm(FlaskForm):
+    def __init__(self, choices, *args, **kwargs):
+        super(RemovePlayerForm, self).__init__(*args, **kwargs)
+        self.players_to_remove.choices = choices
+
+    players_to_remove = SelectMultipleField('Players to remove from league', coerce=int)
+    remove = SubmitField('Remove')
+
+class AddPlayerForm(FlaskForm):
+    def __init__(self, choices, *args, **kwargs):
+        super(AddPlayerForm, self).__init__(*args, **kwargs)
+        self.players_to_add.choices = choices
+
+    players_to_add = SelectMultipleField('Players to add to league', coerce=int)
+    add = SubmitField('Add')
 
 class RegisterPlayerForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
