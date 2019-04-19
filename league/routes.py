56b74e4b065b48f5fd1ab5d105499e906e67e8f2
Daniel Doza <doza.daniel@gmail.com>
2019-04-19 15:06:11 +0200

    matches by phases implemented. 3 phases hardcoded. not working if nPlayers != nPhases^2

--- league/routes.py
+++ league/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, redirect, flash, request
 from flask_login import current_user, login_user, logout_user, login_required
 from math import ceil
 from itertools import combinations
+from functools import reduce
 from datetime import datetime
 from hashlib import sha1
 
@@ -75,9 +76,9 @@ def edit_leagues(league_id):
         return redirect(url_for('home'))
 
     end_form = None
-    groups = None
+    phases = None
     if league.date_started is not None:
-        groups = sum_matches(league)
+        phases = sum_matches(league)
         if all(map(lambda x: x.played_on, league.matches)):
             end_form = EndLeagueForm()
             if end_form.end.data and end_form.validate():
@@ -123,32 +124,50 @@ def edit_leagues(league_id):
             end_form=end_form,
             league=league,
             nplayers=len(to_remove),
-            groups=groups)
+            phases=phases)
 
 # each pair in groups plays two matches, this function groups them by encounter
 # and calculates 'meta' score. for example:
 # p1 and p2 in group g1 have played 2 matches with scores [p1: 7 - 3 :p2] [p1: 7 - 2 :p2]
 # so the meta score will be [p1: 2 - 0 :p2]
 def sum_matches(league):
-    meta = {}
-    for group in league.groups:
-        meta[group] = {}
-        for match in group.matches:
-            key = sha1('-'.join([str(match.player_one.id),str(match.player_two.id)]).encode('utf-8')).hexdigest()
-            p1ms, p2ms = get_meta_score(match)
-            if not meta[group].get(key):
-                meta[group][key] = {"matches":[match], "p1": p1ms, "p2": p2ms}
-            else:
-                meta[group][key]["matches"].append(match)
-                meta[group][key]["p1"] += p1ms
-                meta[group][key]["p2"] += p2ms
-        for key, encounter in meta[group].items():
-            print(encounter)
-            encounter["done"] = all(map(lambda x: x.played_on, encounter["matches"]))
-            encounter["id"] = "-".join(map(lambda x: str(x.id), encounter["matches"]))
+    def hash_player_ids(match):
+        key = sha1('-'.join([str(match.player_one.id),str(match.player_two.id)]).encode('utf-8')).hexdigest()
+        return (key, match)
+
+    def group_by_hash(acc, key_match_pairs):
+        key = key_match_pairs[0]
+        match = key_match_pairs[1]
+
+        print(acc)
+        p1ms, p2ms = get_meta_score(match)
+        if not acc.get(key):
+            acc[key] = {"matches":[match], "p1": p1ms, "p2": p2ms}
+        else:
+            acc[key]["matches"].append(match)
+            acc[key]["p1"] += p1ms
+            acc[key]["p2"] += p2ms
+
+        return acc
+
+    def set_done_and_id(encounter):
+        encounter["done"] = all(map(lambda x: x.played_on, encounter["matches"]))
+        encounter["id"] = "-".join(map(lambda x: str(x.id), encounter["matches"]))
+        return encounter
 
 
-    return meta
+    def add_by_phase(acc, group):
+        hash_match_pairs = map(hash_player_ids, group.matches)
+        matches_by_hash = reduce(group_by_hash, hash_match_pairs, {})
+        grouped_matches = {k: set_done_and_id(v) for k, v in matches_by_hash.items()}
+
+        if not acc.get(group.phase):
+            acc[group.phase] = {group: grouped_matches}
+        else:
+            acc[group.phase][group] = grouped_matches
+        return acc
+
+    return reduce(add_by_phase, league.groups, {})
 
 def get_meta_score(match):
     p1 = 1 if match.player_one_score > match.player_two_score else 0
@@ -160,7 +179,7 @@ def generate_league_matches(league, gsize, num_phases):
     ngroups = ceil(nplayers / gsize)
     groups=[]
     for i in range(ngroups):
-        group = Group(league=league, size=gsize)
+        group = Group(league=league, size=gsize, phase=0)
         groups.append(group)
         db.session.add(group)
 
@@ -172,6 +191,21 @@ def generate_league_matches(league, gsize, num_phases):
         for p1, p2 in combinations(group.players, 2):
             db.session.add(Match(player_one=p1, player_two=p2, league=league, group=group))
             db.session.add(Match(player_one=p1, player_two=p2, league=league, group=group))
+
+    for phase in range(1,3):
+        ng = [Group(league=league, size=gsize, phase=phase) for k in range(ngroups)]
+
+        for i, group in enumerate(ng):
+            for j in range(gsize):
+                if len(groups[j % gsize].players) > (j * (phase - 1) + i) % gsize:
+                    group.players.append(groups[j % gsize].players[(j * (phase - 1) + i) % gsize])
+
+        for group in ng:
+            db.session.add(group)
+            for p1, p2 in combinations(group.players, 2):
+                db.session.add(Match(player_one=p1, player_two=p2, league=league, group=group))
+                db.session.add(Match(player_one=p1, player_two=p2, league=league, group=group))
+
 
     db.session.commit()
 
